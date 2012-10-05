82af4fa02b067e1325312abc2d4813717e2b8fa1
John Koht <john@kohactive.com>
2012-10-05 10:58:56 -0500

    updating the generator

--- lib/generators/bootstrap-sass/variables/variables_generator.rb
+++ lib/generators/bootstrap-sass/variables/variables_generator.rb
@@ -1,8 +1,13 @@
 require "rails/generators"
+
 module BootstrapSass
-  class InstallGenerator < Rails::Generators::Base
+  class VariablesGenerator < Rails::Generators::Base
     desc "This will copy the variables css to the assets pipeline for customization"
     source_root File.expand_path('../../../../../vendor/assets/stylesheets/_variables.css.scss')
-    template 'variables.css.scss', File.join('app/assets/variables.css.scss')
+    
+    def add_css_templates
+      template 'variables.css.scss', 'app/assets/variables.css.scss'
+    end
+    
   end
 end
