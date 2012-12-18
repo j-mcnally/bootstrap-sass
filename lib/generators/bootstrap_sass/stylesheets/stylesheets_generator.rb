6eaf95eee1cff9f55947888ef94c8835e658a0c2
John Koht <john@kohactive.com>
2012-12-17 22:43:16 -0600

    removing rake task and adding to generators

--- lib/generators/bootstrap_sass/stylesheets/stylesheets_generator.rb
+++ lib/generators/bootstrap_sass/stylesheets/stylesheets_generator.rb
@@ -1,18 +1,15 @@
 require "rails/generators"
+
 module BootstrapSass
   class StylesheetsGenerator < Rails::Generators::Base
+
     desc "This will generate a copy of a given css file to the assets pipeline for customization."
+    source_root File.expand_path('../../../../../vendor/assets/stylesheets/', __FILE__)
     argument :stylesheet_name, :default => "variables"
-    puts "stylesheet_name: #{stylesheet_name}"
-    # If the requested stylesheet is bootstrap or bootstrap-responsive, we need to look in a different folder
-    if stylesheet_name.downcase == "bootstrap" or stylesheet_name.downcase == "bootstrap-responsive"
-      source_root File.expand_path('../../../../../vendor/assets/stylesheets/', __FILE__)
-    else
-      source_root File.expand_path('../../../../../vendor/assets/stylesheets/bootstrap/', __FILE__)
-    end
-    # Copy the file
+    
     def add_css_templates
-      copy_file "_#{stylesheet_name}.scss", "app/assets/stylesheets/#{stylesheet_name}.css.scss"
+      copy_file "_#{stylesheet_name.underscore}.scss", "app/assets/stylesheets/#{stylesheet_name.underscore}.css.scss"
     end
+    
   end
 end
