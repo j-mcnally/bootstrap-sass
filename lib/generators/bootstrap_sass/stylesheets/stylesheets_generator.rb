c5a2f30408ed5ae5cb7c589cb761cbd0ac314c18
John Koht <john@kohactive.com>
2013-01-15 20:07:43 -0600

    fixing the generator again..dammit

--- lib/generators/bootstrap_sass/stylesheets/stylesheets_generator.rb
+++ lib/generators/bootstrap_sass/stylesheets/stylesheets_generator.rb
@@ -4,9 +4,9 @@ module BootstrapSass
   class StylesheetsGenerator < Rails::Generators::Base
 
     desc "This will generate a copy of a given css file to the assets pipeline for customization."
+    argument :stylesheet_name, :default => "variables"
     setup = (stylesheet_name == "bootstrap" or stylesheet_name == "main" or stylesheet_name == "setup") ? true : false
     source_root File.expand_path("../../../../../vendor/assets/stylesheets/#{"bootstrap/" unless setup}", __FILE__)
-    argument :stylesheet_name, :default => "variables"
     
     def add_css_templates
       copy_file "_#{stylesheet_name}.scss", "app/assets/stylesheets/#{stylesheet_name}.css.scss"
