654fd7f08a89fcec6306ed5c00cc1d23c28b74f6
John Koht <john@kohactive.com>
2013-01-15 20:01:39 -0600

    fixing the generator and version bump

--- lib/generators/bootstrap_sass/stylesheets/stylesheets_generator.rb
+++ lib/generators/bootstrap_sass/stylesheets/stylesheets_generator.rb
@@ -8,7 +8,8 @@ module BootstrapSass
     argument :stylesheet_name, :default => "variables"
     
     def add_css_templates
-      copy_file "_#{stylesheet_name}.scss", "app/assets/stylesheets/#{stylesheet_name}.css.scss"
+      setup = (stylesheet_name == "bootstrap" or stylesheet_name == "main" or stylesheet_name == "setup") ? true : false
+      copy_file "_#{stylesheet_name}.scss", "app/assets/stylesheets/#{"bootstrap/" unless setup}#{stylesheet_name}.css.scss"
     end
     
   end
